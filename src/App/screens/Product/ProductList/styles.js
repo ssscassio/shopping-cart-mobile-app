73255cbfdc8e62dbd56203dd0d57fc97ac74cd87
Cássio Santos <sss.cassio@gmail.com>
2019-04-03 09:30:38 -0300

    :sparkles: List products with local mockup

---
+++ src/App/screens/Product/ProductList/styles.js
@@ -0,0 +1,13 @@
+import colors from '../../../config/colors';
+
+export default {
+  container: {
+    backgroundColor: colors.background,
+    flex: 1,
+  },
+  welcome: {
+    fontSize: 20,
+    margin: 10,
+    textAlign: 'center',
+  },
+};
