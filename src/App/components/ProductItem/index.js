9446f61faf73f1ae12a7513aa465e9bfb1a81633
Cássio Santos <sss.cassio@gmail.com>
2019-04-05 16:24:27 -0300

    :sparkles: Connect ProductItem to store

--- src/App/components/ProductItem/index.js
+++ src/App/components/ProductItem/index.js
@@ -3,33 +3,23 @@
  * @flow
  */
 import React from 'react';
+import { connect } from 'react-redux';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 
+import type { itemType } from '../../flow/types';
+import { addOneItem } from '../../store/actions';
 import Button from '../Button';
 import formatCurrency from '../../util';
 import styles from './styles';
 
 type Props = {
-  navigate: (item: {
-    id: string,
-    description: string,
-    price: number,
-    available: number,
-    picture: string,
-    title: string,
-  }) => void,
-  item: {
-    id: string,
-    description: string,
-    price: number,
-    available: number,
-    picture: string,
-    title: string,
-  },
+  navigate: (item: itemType) => void,
+  item: itemType,
+  addOne: (item: itemType) => mixed,
 };
 
 const ProductItem = (props: Props) => {
-  const { navigate } = props;
+  const { navigate, item, addOne } = props;
   const {
     item: { picture, price, title },
   } = props;
@@ -44,11 +34,27 @@ const ProductItem = (props: Props) => {
         <Text style={styles.productTitle}>{title}</Text>
         <View style={styles.priceContainer}>
           <Text style={styles.productPrice}>{formatCurrency(price)}</Text>
-          <Button withIcon iconName="add-shopping-cart" text="Add to Cart" onPress={() => {}} />
+          <Button
+            withIcon
+            iconName="add-shopping-cart"
+            text="Add to Cart"
+            onPress={() => {
+              addOne(item);
+            }}
+          />
         </View>
       </View>
     </TouchableOpacity>
   );
 };
 
-export default ProductItem;
+const mapDispatchToProps = dispatch => ({
+  addOne: item => dispatch(addOneItem(item)),
+});
+
+export { ProductItem };
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(ProductItem);
