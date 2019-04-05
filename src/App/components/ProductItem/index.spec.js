bcf34cd4a8c80c6bea8c50c4f82b924658100640
Cássio Santos <sss.cassio@gmail.com>
2019-04-05 16:24:27 -0300

    :sparkles: Connect ProductItem to store

--- src/App/components/ProductItem/index.spec.js
+++ src/App/components/ProductItem/index.spec.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import ProductItem from '.';
+import { ProductItem } from '.';
 
 describe('<ProductItem />', () => {
   const wine = {
