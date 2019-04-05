0863b4ba85914e31ef414353c62816d279fde480
Cássio Santos <sss.cassio@gmail.com>
2019-04-04 23:50:05 -0300

    :lipstick: Update ProductItem

--- src/App/screens/ProductList/index.js
+++ src/App/screens/ProductList/index.js
@@ -17,7 +17,7 @@ class ProductList extends Component<Props> {
     id: string,
     description: string,
     value: number,
-    qtd: number,
+    available: number,
     picture: string,
     title: string,
   }) => {
@@ -29,7 +29,7 @@ class ProductList extends Component<Props> {
     id: string,
     description: string,
     value: number,
-    qtd: number,
+    available: number,
     picture: string,
     title: string,
   }): string => item.id;
@@ -41,7 +41,7 @@ class ProductList extends Component<Props> {
       description:
         'Análise Sensorial: \n Análise Visual: cor rosa com reflexos alaranjados. Análise Olfativa: Frutas vermelhas (morango) Análise Gustativa: seco, corpo médio, excelente acidez, taninos de boa qualidade, com retro-olfato de frutas.',
       value: 200,
-      qtd: 20,
+      available: 20,
       picture: 'https://cdn.awsli.com.br/600x700/365/365010/produto/13341816/fe9a59131b.jpg',
       title: 'Vinho Nederburg Rosé',
     };
