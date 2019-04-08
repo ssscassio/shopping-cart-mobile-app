d60d6102c06f2e5b80f76f2af9207dba553d97c5
Cássio Santos <sss.cassio@gmail.com>
2019-04-08 15:07:40 -0300

    :sparkles: Add Pagination

--- src/App/screens/ProductList/index.js
+++ src/App/screens/ProductList/index.js
@@ -17,15 +17,17 @@ import { View, FlatList } from 'react-native';
 import type { ProductListProps } from 'react-navigation';
 import type { itemType } from '../../flow/types';
 
-import { fetchItemsIfNeeded } from '../../store/actions';
+import { fetchItemsIfNeeded, refreshStoreList } from '../../store/actions';
 import ProductItem from '../../components/ProductItem';
 import styles from './styles';
 
 class ProductList extends Component<ProductListProps> {
   componentDidMount() {
-    const { fetchItems } = this.props;
+    const { fetchItems, products } = this.props;
     // Fetch Items from GraphQl API if needed
-    fetchItems();
+    if (!products.length) {
+      fetchItems();
+    }
   }
 
   navigateToDetails = (item: itemType) => {
@@ -36,13 +38,17 @@ class ProductList extends Component<ProductListProps> {
   _keyExtractor = (item: itemType): string => item.id;
 
   render() {
-    const { products, loading, fetchItems } = this.props;
+    const { products, loading, fetchItems, refreshStore } = this.props;
     return (
       <View style={styles.container}>
         <FlatList
           data={products}
           refreshing={loading} // Display activity Indicator while fetching
-          onRefresh={fetchItems} // Fetch items on pull down
+          onRefresh={() => {
+            refreshStore();
+            fetchItems();
+          }}
+          onEndReached={fetchItems} // Fetch items when reached end
           keyExtractor={this._keyExtractor}
           renderItem={({ item }) => (
             <ProductItem item={item} navigate={this.navigateToDetails} key={item.id} />
@@ -62,6 +68,7 @@ const mapStateToProps = state => ({
 // Map Redux Actions Dispatchers to Component Props
 const mapDispatchToProps = dispatch => ({
   fetchItems: () => dispatch(fetchItemsIfNeeded()),
+  refreshStore: () => dispatch(refreshStoreList()),
 });
 
 // Connect component with store and export it as default
--- src/App/store/reducers/store.js
+++ src/App/store/reducers/store.js
@@ -4,25 +4,27 @@
  * @format
  * @flow
  */
-import { REQUEST_ALL_ITEMS, RECEIVE_ITEMS } from '../actions/store';
+import { REQUEST_ITEMS, RECEIVE_ITEMS, RECEIVE_ITEMS_WITH_OFFSET } from '../actions/store';
 import type { itemType } from '../../flow/types';
 
 // Store initial state
 const initialState = {
   isFetching: false,
   items: [],
+  offset: 0,
 };
 
 type stateType = {
   isFetching: boolean,
   items: Array<itemType>,
+  offset: number,
 };
 const store = (
   state: stateType = initialState,
-  action: { type: string, items: Array<itemType> }
+  action: { type: string, items: Array<itemType>, offset: number }
 ) => {
   switch (action.type) {
-    case REQUEST_ALL_ITEMS: {
+    case REQUEST_ITEMS: {
       return Object.assign({}, state, {
         isFetching: true,
       });
@@ -31,6 +33,12 @@ const store = (
       return Object.assign({}, state, {
         isFetching: false,
         items: action.items,
+      });
+    }
+    case RECEIVE_ITEMS_WITH_OFFSET: {
+      return Object.assign({}, state, {
+        isFetching: false,
+        items: [...state.items, ...action.items],
       });
     }
     default: {
