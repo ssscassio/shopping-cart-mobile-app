65ed168d74a7943146939a4056975cda25f4cb5e
Cássio Santos <sss.cassio@gmail.com>
2019-04-08 21:10:30 -0300

    Add compiled files

--- src/App/__generated__/AppQuery.graphql.js
+++ src/App/__generated__/AppQuery.graphql.js
@@ -21,7 +21,6 @@ export type AppQuery = {|
 |};
 */
 
-
 /*
 query AppQuery {
   allItems(first: 10) {
@@ -30,58 +29,58 @@ query AppQuery {
 }
 */
 
-const node/*: ConcreteRequest*/ = (function(){
-var v0 = [
-  {
-    "kind": "LinkedField",
-    "alias": null,
-    "name": "allItems",
-    "storageKey": "allItems(first:10)",
-    "args": [
-      {
-        "kind": "Literal",
-        "name": "first",
-        "value": 10,
-        "type": "Int"
-      }
-    ],
-    "concreteType": "Item",
-    "plural": true,
-    "selections": [
-      {
-        "kind": "ScalarField",
-        "alias": null,
-        "name": "id",
-        "args": null,
-        "storageKey": null
-      }
-    ]
-  }
-];
-return {
-  "kind": "Request",
-  "fragment": {
-    "kind": "Fragment",
-    "name": "AppQuery",
-    "type": "Query",
-    "metadata": null,
-    "argumentDefinitions": [],
-    "selections": (v0/*: any*/)
-  },
-  "operation": {
-    "kind": "Operation",
-    "name": "AppQuery",
-    "argumentDefinitions": [],
-    "selections": (v0/*: any*/)
-  },
-  "params": {
-    "operationKind": "query",
-    "name": "AppQuery",
-    "id": null,
-    "text": "query AppQuery {\n  allItems(first: 10) {\n    id\n  }\n}\n",
-    "metadata": {}
-  }
-};
+const node /*: ConcreteRequest*/ = (function() {
+  var v0 = [
+    {
+      kind: 'LinkedField',
+      alias: null,
+      name: 'allItems',
+      storageKey: 'allItems(first:10)',
+      args: [
+        {
+          kind: 'Literal',
+          name: 'first',
+          value: 10,
+          type: 'Int',
+        },
+      ],
+      concreteType: 'Item',
+      plural: true,
+      selections: [
+        {
+          kind: 'ScalarField',
+          alias: null,
+          name: 'id',
+          args: null,
+          storageKey: null,
+        },
+      ],
+    },
+  ];
+  return {
+    kind: 'Request',
+    fragment: {
+      kind: 'Fragment',
+      name: 'AppQuery',
+      type: 'Query',
+      metadata: null,
+      argumentDefinitions: [],
+      selections: v0 /*: any*/,
+    },
+    operation: {
+      kind: 'Operation',
+      name: 'AppQuery',
+      argumentDefinitions: [],
+      selections: v0 /*: any*/,
+    },
+    params: {
+      operationKind: 'query',
+      name: 'AppQuery',
+      id: null,
+      text: 'query AppQuery {\n  allItems(first: 10) {\n    id\n  }\n}\n',
+      metadata: {},
+    },
+  };
 })();
 // prettier-ignore
 (node/*: any*/).hash = '476ae010a80f1fe4021f5cbe2e1b5b77';
--- src/App/screens/ProductList/__generated__/ProductListContainerQuery.graphql.js
+++ src/App/screens/ProductList/__generated__/ProductListContainerQuery.graphql.js
@@ -23,7 +23,6 @@ export type ProductListContainerQuery = {|
 |};
 */
 
-
 /*
 query ProductListContainerQuery {
   allItems(first: 10) {
@@ -42,112 +41,113 @@ fragment ProductItem_item on Item {
 }
 */
 
-const node/*: ConcreteRequest*/ = (function(){
-var v0 = [
-  {
-    "kind": "Literal",
-    "name": "first",
-    "value": 10,
-    "type": "Int"
-  }
-],
-v1 = {
-  "kind": "ScalarField",
-  "alias": null,
-  "name": "id",
-  "args": null,
-  "storageKey": null
-};
-return {
-  "kind": "Request",
-  "fragment": {
-    "kind": "Fragment",
-    "name": "ProductListContainerQuery",
-    "type": "Query",
-    "metadata": null,
-    "argumentDefinitions": [],
-    "selections": [
+const node /*: ConcreteRequest*/ = (function() {
+  var v0 = [
       {
-        "kind": "LinkedField",
-        "alias": null,
-        "name": "allItems",
-        "storageKey": "allItems(first:10)",
-        "args": (v0/*: any*/),
-        "concreteType": "Item",
-        "plural": true,
-        "selections": [
-          (v1/*: any*/),
-          {
-            "kind": "FragmentSpread",
-            "name": "ProductItem_item",
-            "args": null
-          }
-        ]
-      }
-    ]
-  },
-  "operation": {
-    "kind": "Operation",
-    "name": "ProductListContainerQuery",
-    "argumentDefinitions": [],
-    "selections": [
-      {
-        "kind": "LinkedField",
-        "alias": null,
-        "name": "allItems",
-        "storageKey": "allItems(first:10)",
-        "args": (v0/*: any*/),
-        "concreteType": "Item",
-        "plural": true,
-        "selections": [
-          (v1/*: any*/),
-          {
-            "kind": "ScalarField",
-            "alias": null,
-            "name": "picture",
-            "args": null,
-            "storageKey": null
-          },
-          {
-            "kind": "ScalarField",
-            "alias": null,
-            "name": "price",
-            "args": null,
-            "storageKey": null
-          },
-          {
-            "kind": "ScalarField",
-            "alias": null,
-            "name": "title",
-            "args": null,
-            "storageKey": null
-          },
-          {
-            "kind": "ScalarField",
-            "alias": null,
-            "name": "available",
-            "args": null,
-            "storageKey": null
-          },
-          {
-            "kind": "ScalarField",
-            "alias": null,
-            "name": "description",
-            "args": null,
-            "storageKey": null
-          }
-        ]
-      }
-    ]
-  },
-  "params": {
-    "operationKind": "query",
-    "name": "ProductListContainerQuery",
-    "id": null,
-    "text": "query ProductListContainerQuery {\n  allItems(first: 10) {\n    id\n    ...ProductItem_item\n  }\n}\n\nfragment ProductItem_item on Item {\n  picture\n  price\n  title\n  available\n  description\n  id\n}\n",
-    "metadata": {}
-  }
-};
+        kind: 'Literal',
+        name: 'first',
+        value: 10,
+        type: 'Int',
+      },
+    ],
+    v1 = {
+      kind: 'ScalarField',
+      alias: null,
+      name: 'id',
+      args: null,
+      storageKey: null,
+    };
+  return {
+    kind: 'Request',
+    fragment: {
+      kind: 'Fragment',
+      name: 'ProductListContainerQuery',
+      type: 'Query',
+      metadata: null,
+      argumentDefinitions: [],
+      selections: [
+        {
+          kind: 'LinkedField',
+          alias: null,
+          name: 'allItems',
+          storageKey: 'allItems(first:10)',
+          args: v0 /*: any*/,
+          concreteType: 'Item',
+          plural: true,
+          selections: [
+            v1 /*: any*/,
+            {
+              kind: 'FragmentSpread',
+              name: 'ProductItem_item',
+              args: null,
+            },
+          ],
+        },
+      ],
+    },
+    operation: {
+      kind: 'Operation',
+      name: 'ProductListContainerQuery',
+      argumentDefinitions: [],
+      selections: [
+        {
+          kind: 'LinkedField',
+          alias: null,
+          name: 'allItems',
+          storageKey: 'allItems(first:10)',
+          args: v0 /*: any*/,
+          concreteType: 'Item',
+          plural: true,
+          selections: [
+            v1 /*: any*/,
+            {
+              kind: 'ScalarField',
+              alias: null,
+              name: 'picture',
+              args: null,
+              storageKey: null,
+            },
+            {
+              kind: 'ScalarField',
+              alias: null,
+              name: 'price',
+              args: null,
+              storageKey: null,
+            },
+            {
+              kind: 'ScalarField',
+              alias: null,
+              name: 'title',
+              args: null,
+              storageKey: null,
+            },
+            {
+              kind: 'ScalarField',
+              alias: null,
+              name: 'available',
+              args: null,
+              storageKey: null,
+            },
+            {
+              kind: 'ScalarField',
+              alias: null,
+              name: 'description',
+              args: null,
+              storageKey: null,
+            },
+          ],
+        },
+      ],
+    },
+    params: {
+      operationKind: 'query',
+      name: 'ProductListContainerQuery',
+      id: null,
+      text:
+        'query ProductListContainerQuery {\n  allItems(first: 10) {\n    id\n    ...ProductItem_item\n  }\n}\n\nfragment ProductItem_item on Item {\n  picture\n  price\n  title\n  available\n  description\n  id\n}\n',
+      metadata: {},
+    },
+  };
 })();
 // prettier-ignore
 (node/*: any*/).hash = 'c466ee74d0b2dc15ca26872fec874a12';
