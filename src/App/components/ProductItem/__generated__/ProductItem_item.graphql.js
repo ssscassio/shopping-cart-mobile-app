82ff98b659c0621c6941135bbea61f5c4c8edc3b
Cássio Santos <sss.cassio@gmail.com>
2019-04-10 09:45:55 -0300

    Update babel

--- src/App/components/ProductItem/__generated__/ProductItem_item.graphql.js
+++ src/App/components/ProductItem/__generated__/ProductItem_item.graphql.js
@@ -21,56 +21,57 @@ export type ProductItem_item = {|
 |};
 */
 
-const node /*: ReaderFragment*/ = {
-  kind: 'Fragment',
-  name: 'ProductItem_item',
-  type: 'Item',
-  metadata: null,
-  argumentDefinitions: [],
-  selections: [
+
+const node/*: ReaderFragment*/ = {
+  "kind": "Fragment",
+  "name": "ProductItem_item",
+  "type": "Item",
+  "metadata": null,
+  "argumentDefinitions": [],
+  "selections": [
     {
-      kind: 'ScalarField',
-      alias: null,
-      name: 'picture',
-      args: null,
-      storageKey: null,
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "picture",
+      "args": null,
+      "storageKey": null
     },
     {
-      kind: 'ScalarField',
-      alias: null,
-      name: 'price',
-      args: null,
-      storageKey: null,
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "price",
+      "args": null,
+      "storageKey": null
     },
     {
-      kind: 'ScalarField',
-      alias: null,
-      name: 'title',
-      args: null,
-      storageKey: null,
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "title",
+      "args": null,
+      "storageKey": null
     },
     {
-      kind: 'ScalarField',
-      alias: null,
-      name: 'available',
-      args: null,
-      storageKey: null,
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "available",
+      "args": null,
+      "storageKey": null
     },
     {
-      kind: 'ScalarField',
-      alias: null,
-      name: 'description',
-      args: null,
-      storageKey: null,
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "description",
+      "args": null,
+      "storageKey": null
     },
     {
-      kind: 'ScalarField',
-      alias: null,
-      name: 'id',
-      args: null,
-      storageKey: null,
-    },
-  ],
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "id",
+      "args": null,
+      "storageKey": null
+    }
+  ]
 };
 // prettier-ignore
 (node/*: any*/).hash = 'c5a9256903ca860703e9fc5fce8a2075';
